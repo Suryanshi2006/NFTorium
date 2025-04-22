8812f0a6a86f8e72a49742fb8a10175263260e64
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-04-22 14:51:22 +0200

    refactor: changed attribute class to className

--- src/components/NFT.jsx
+++ src/components/NFT.jsx
@@ -146,33 +146,33 @@ const NFT = () => {
 
   return (
     <div className="px-16 py-20 bg-black ">
-      <section class=" bg-white md:py-16 dark:bg-black antialiased">
-        <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-          <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
-            <div class="shrink-0 max-w-md lg:max-w-lg mx-auto">
-              <img class="w-full dark:hidden" src={nftimg} alt="" />
-              <img class="w-full hidden dark:block" src={nftimg} alt="" />
+      <section className="antialiased bg-white md:py-16 dark:bg-black">
+        <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+          <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+            <div className="max-w-md mx-auto shrink-0 lg:max-w-lg">
+              <img className="w-full dark:hidden" src={nftimg} alt="" />
+              <img className="hidden w-full dark:block" src={nftimg} alt="" />
             </div>
 
-            <div class="mt-6 sm:mt-8 lg:mt-0">
-              <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+            <div className="mt-6 sm:mt-8 lg:mt-0">
+              <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
                 {name}
               </h1>
-              <div class="mt-4 sm:items-center sm:gap-4 sm:flex">
-                <p class="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
+              <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
+                <p className="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
                   {displayPrice} ETH
                 </p>
               </div>
 
-              <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+              <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
                 <a
                   href="#"
                   title=""
-                  class="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                   role="button"
                 >
                   <svg
-                    class="w-5 h-5 -ms-2 me-2"
+                    className="w-5 h-5 -ms-2 me-2"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -194,11 +194,11 @@ const NFT = () => {
                 <a
                   href="#"
                   title=""
-                  class="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                  className="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
                   role="button"
                 >
                   <svg
-                    class="w-5 h-5 -ms-2 me-2"
+                    className="w-5 h-5 -ms-2 me-2"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -279,10 +279,10 @@ const NFT = () => {
 
               {message == "Listed" ? (
                 <div
-                  class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+                  className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
                   role="alert"
                 >
-                  <span class="font-medium">Wait !</span> Transfering ownership
+                  <span className="font-medium">Wait !</span> Transfering ownership
                 </div>
               ) : (
                 <></>
@@ -303,11 +303,11 @@ const NFT = () => {
                   {message}
                 </div>
               )}
-              <hr class="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
+              <hr className="my-6 border-gray-200 md:my-8 dark:border-gray-800" />
 
-              <p class="mb-6 text-gray-500 dark:text-gray-400">{desc}</p>
+              <p className="mb-6 text-gray-500 dark:text-gray-400">{desc}</p>
 
-              <p class="text-gray-500 dark:text-gray-400">
+              <p className="text-gray-500 dark:text-gray-400">
                 <div>Owner : {owner} </div>
                 <div>Seller : {seller} </div>
               </p>
